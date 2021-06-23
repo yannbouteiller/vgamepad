9a06937c1289c3af5a15e5d56c76532d05afed3c
yannbouteiller <yann.bouteiller@hotmail.fr>
2021-06-23 14:24:26 -0400

    release 0.0.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description_content_type="text/markdown",
     author='Yann Bouteiller',
     url='https://github.com/yannbouteiller/vgamepad',
-    download_url='https://github.com/yannbouteiller/vgamepad/archive/v0.0.3.tar.gz',
+    download_url='https://github.com/yannbouteiller/vgamepad/archive/refs/tags/v0.0.3.tar.gz',
     keywords=['virtual', 'gamepad', 'python', 'xbox', 'dualshock', 'controller', 'emulator'],
     install_requires=[],
     classifiers=[
