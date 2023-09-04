499acdaea4991cd3e6917e1fc9cc6471c3bca9d8
Guillaume RICARD <guillaume.ricard@mail.com>
2023-09-04 18:53:01 -0400

    fix(linux): Rename dir for consistency
    Use shorter OS subpackage names for a consistent experience.

--- vgamepad/__init__.py
+++ vgamepad/__init__.py
@@ -4,4 +4,4 @@ from vgamepad.win.vigem_commons import VIGEM_TARGET_TYPE, XUSB_BUTTON, DS4_BUTTO
 if os.name == 'nt':
     from vgamepad.win.virtual_gamepad import VX360Gamepad, VDS4Gamepad
 else:
-    from vgamepad.linux.virtual_gamepad import VX360Gamepad, VDS4Gamepad
+    from vgamepad.lin.virtual_gamepad import VX360Gamepad, VDS4Gamepad
